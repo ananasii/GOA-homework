3cf7e0bf0a895cce525c003f54e4899e5bbd1536
ananasii <pinkpandaana@gmail.com>
2024-02-18 00:15:51 +0400

    second commit

---
+++ day 2/lesson 3.py
@@ -0,0 +1,5 @@
+whatithinkof_goa = "The mentor I got is very helpful and the vidoes have been very helpful to make these codes. It has been very fun to learn. However there is a reacurring problem, the problem being that in the youtube videos the teachers voice can not be heard properly even on the highest volume." 
+
+goa_friends = " Sadly, I haven't been able to talk to anyone, because of my tight schedule. I am very busy at the moment, but if I get the chance I will join the voice chats."
+
+print((whatithinkof_goa) + " " + str(goa_friends))
