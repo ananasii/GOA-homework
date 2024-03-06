71d614492deb5cbd59054e20813e16b70b148a32
ananasii <pinkpandaana@gmail.com>
2024-03-06 23:38:59 +0400

    commit

---
+++ day 5/day 5 p2 .py
@@ -0,0 +1,52 @@
+number1= int(input("enter a number: "))
+number2= int(float("enter a second number: "))
+
+print(type(number1))
+print(type(number2))
+
+print(number1 / number2)
+print(number1 // number2)
+print(number1 % number2)
+
+number3= int(float("enter a third number: "))
+number4= int(input("please enter the fourth number: "))
+
+print(type(number3))
+print(type(number4))
+
+print(number3 / number2)
+print(number3 // number2)
+print(number3 % number2)
+
+print(number1 / number4)
+print(number1 // number4)
+print(number1 % number4)
+
+print(number1 / number3)
+print(number1 // number3)
+print(number1 % number3)
+
+print(number4 / number2)
+print(number4 // number2)
+print(number4 % number2)
+
+
+print(number4 / number3)
+print(number4 // number3)
+print(number4 % number3)
+
+number5= int(input("enter the fifth number: "))
+number6=  int(input("enter the sixth number: "))
+number7= int(float("please enter the seventh number: "))
+
+print(number6 / number5)
+print(number6 // number5)
+print(number6 % number5)
+
+print(number6 / number7)
+print(number6 // number7)
+print(number6 % number7)
+
+print(number7 / number4)
+print(number7 // number4)
+print(number7 % number4)
---
+++ day 5/day 5.py
@@ -0,0 +1,5 @@
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter a second number: "))
+
+print(type(number1))
+print(type(number2))
